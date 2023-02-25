aa767ad6ef16bf88e9e3e956f0a21dfe764a9978
TacoMaster151 <115062137+TacoMaster151@users.noreply.github.com>
2023-02-24 17:26:15 -0800

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('button');
+const screenDisplay = document.querySelector('.screen');
+
+let calculation = [];
+
+function calculate(button) {
+    const value = button.textContent 
+    if (value === "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '0'
+    } else if (value === "=") {
+        console.log(accumulativeCalculation)
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value);
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
